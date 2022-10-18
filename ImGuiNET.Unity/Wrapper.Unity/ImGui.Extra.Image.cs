7254ce41492010eb59ff511078d2a9f12d693164
chao3 <chao3@rayark.com>
2022-10-18 21:50:13 +0800

    Expose image button click result

--- ImGuiNET.Unity/Wrapper.Unity/ImGui.Extra.Image.cs
+++ ImGuiNET.Unity/Wrapper.Unity/ImGui.Extra.Image.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Runtime.CompilerServices;
+
 using UnityEngine;
+
 using ImGuiNET.Unity;
 
 namespace ImGuiNET
@@ -10,15 +12,11 @@ namespace ImGuiNET
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Image(Texture tex)
-        {
-            ImGui.Image((IntPtr)GetTextureId(tex), new Vector2(tex.width, tex.height));
-        }
+            => ImGui.Image((IntPtr)GetTextureId(tex), new Vector2(tex.width, tex.height));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Image(Texture tex, Vector2 size)
-        {
-            ImGui.Image((IntPtr)GetTextureId(tex), size);
-        }
+            => ImGui.Image((IntPtr)GetTextureId(tex), size);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Image(Sprite sprite)
@@ -36,29 +34,25 @@ namespace ImGuiNET
 
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ImageButton(Texture tex)
-        {
-            ImGui.ImageButton((IntPtr)GetTextureId(tex), new Vector2(tex.width, tex.height));
-        }
+        public static bool ImageButton(Texture tex)
+            => ImGui.ImageButton((IntPtr)GetTextureId(tex), new Vector2(tex.width, tex.height));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ImageButton(Texture tex, Vector2 size)
-        {
-            ImGui.ImageButton((IntPtr)GetTextureId(tex), size);
-        }
+        public static bool ImageButton(Texture tex, Vector2 size)
+            => ImGui.ImageButton((IntPtr)GetTextureId(tex), size);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ImageButton(Sprite sprite)
+        public static bool ImageButton(Sprite sprite)
         {
             SpriteInfo info = GetSpriteInfo(sprite);
-            ImGui.ImageButton((IntPtr)GetTextureId(info.texture), info.size, info.uv0, info.uv1);
+            return ImGui.ImageButton((IntPtr)GetTextureId(info.texture), info.size, info.uv0, info.uv1);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ImageButton(Sprite sprite, Vector2 size)
+        public static bool ImageButton(Sprite sprite, Vector2 size)
         {
             SpriteInfo info = GetSpriteInfo(sprite);
-            ImGui.ImageButton((IntPtr)GetTextureId(info.texture), size, info.uv0, info.uv1);
+            return ImGui.ImageButton((IntPtr)GetTextureId(info.texture), size, info.uv0, info.uv1);
         }
     }
 }
